a196c7d617d6fe6aa3674f65594b65434c4fcb3d
Suryakanta Swain <suryakantaswain606@gmail.com>
2025-09-16 22:39:17 +0530

    added next()

--- src/app.js
+++ src/app.js
@@ -1,15 +1,27 @@
 const express = require("express");
 const app = express();
 
-app.use("/shop", (req, res) => {
-  res.send("thanks for using our service");
-});
-app.use("/cart", (req, res) => {
-  res.send("cart is empty");
-});
-app.use("/list", (req, res) => {
-  res.send("list is full");
-});
+app.get(
+  "/user",
+  [
+    (req, res, next) => {
+      next();
+      // res.send("Route Handler 1");
+    },
+    (req, res, next) => {
+      // res.send("2");
+      next();
+    },
+  ],
+  (req, res, next) => {
+    // res.send("3");
+    next();
+  },
+  (req, res, next) => {
+    res.send("4");
+    // next();
+  }
+);
 
 app.listen(4000, () => {
   console.log("listening");
